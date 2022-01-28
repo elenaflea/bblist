d16f8702327c390ffa02b45bbb0d5589ea201d01
Elena GRONDIN <88861549+elenaflea@users.noreply.github.com>
2022-01-28 15:42:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,168 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo2.png';
+import mylogo from './mylogo.png'
+import bear from './bear.png'
+import { useState } from 'react';
+import { data } from './data';
+
+
 
 function App() {
+
+  const [gifts, setGifts] = useState(data);
+
+  const removeItem = (id) => {
+    let newGifts = gifts.filter(element => element.id !== id);
+    setGifts(newGifts);
+  };
+
+  const [t, setT] = useState(0);
+  const [datas, setDatas] = useState(data);
+  let newArr = [...datas];
+  
+  const [count, setCount] = useState([0,0,0,0,0,0,0,0,0,0]);
+  
+  const nextPhoto = (e, index) => {
+    setDatas(newArr);
+
+     // if ( e.currentTarget.id - 1 === index ) {
+        const slide = document.querySelectorAll("#slide");
+       // console.log(slide[index]);
+
+      //let newArr = [...datas];
+     // newArr[index] = gifts[index];
+    
+      // setDatas(newArr);
+     // console.log(newArr[index]);
+      setT ((t => {
+        t ++ ;
+        if (t > 2 ) {
+        t = 0;
+        }
+        return t;
+        
+      }));
+      slide[index].setAttribute('src', newArr[index].image[t] );
+      return slide;
+      //}
+      
+    };
+
+    const prevPhoto = (e, index) => {
+     // if ( e.currentTarget.id - 1 === index ) {
+        const slide = document.querySelectorAll("#slide");
+       // let newArr = [...datas];
+       // newArr[index] = gifts[index];
+    
+        setDatas(newArr);
+        setT ((t => {
+        t -- ;
+        if (t <0 ) {
+        t = 2;
+        }
+        return t;
+      }));
+
+      slide[index].setAttribute('src', newArr[index].image[t] );
+      return slide;
+     // }
+    };
+
+  const likeUpdate = (e, index) => {
+  const likeHolder = document.querySelectorAll("#likeHolder");
+    setCount ( ( count => {
+
+      count[index] ++ ; 
+      return count;
+      }) ) 
+   // console.log(count);
+    likeHolder[index].innerHTML = (gifts[index].like) + count[index] ;
+    
+    
+    
+    return likeHolder;
+    
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <br />
+      <br />
+      <img src={ bear } width="50px" alt="icon bear"/>
+      <br />
+      <h2>La Liste des</h2>
+      <h1>{ gifts.length } INDISPENSABLES</h1>
+      <h2> à acheter avant la naissance</h2>
+      <img src={ logo } className='App-logo' alt='logo'/>
+
+    {gifts.map((element => {
+        const {id, gifty, image, like} = element;
+        const index = gifts.indexOf(element);
+
+        return (
+          <div className='item' key={ id } data-id={data.id}>
+            <div className='string'>
+              <hr />
+                <h3> { id } </h3>
+              <hr />
+            </div>
+          <img id="slide" className='photo' src={ gifts[index].image[3] } alt='photo'/>
+          <div> 
+            <button className='btnSlide' onClick={ (e)=> prevPhoto(e, index)  } id = { id } > 
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"  viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+            </svg>
+            </button>
+            <button className='btnSlide' onClick={ (e)=> nextPhoto(e, index)} id = { id } >  
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"  viewBox="0 0 16 16" >
+              <path  fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+              </svg> 
+            </button>
+          </div>
+
+          <p className='giftyName'>
+            { gifty }
+          </p>
+          <div className='string'>
+
+          <p className='numberLike' id="likeHolder" value = ""> 
+            { like }
+          </p>
+
+          <button className='btnLike' id = { id }
+          onClick={ (e)=> likeUpdate(e, index)}
+          >
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"  viewBox="0 0 16 16">
+            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+          </svg>
+          </button>
+
+            <button className='btnRemove' onClick={ ()=> removeItem(id) }>
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor"  viewBox="0 0 16 16">
+                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+              </svg>
+            </button>
+          </div>
+          <br/> <br/>
+          </div>
+          
+        )
+          
+    }))}
+
+<button className='btnDeleteAll' onClick={ () => setGifts([]) }> Delete all </button>
+
+        <a href='https://grondin.glitch.me/' target="_blank" rel="noreferrer">
+          <img className='myIcon' src={ mylogo } alt="logo"/>
         </a>
-      </header>
+
+
     </div>
+
+
+   
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,104 @@
+import lit1 from './lit2.jpg';
+import lit2 from './lit3.jpg';
+import lit3 from './lit.jpg';
+import lit4 from './lit4.jpg';
+import table1 from './table.jpg';
+import table2 from './table2.jpg';
+import table3 from './table3.jpg';
+import table4 from './table4.jpg';
+import siege1 from './siege.jpg';
+import siege2 from './siege2.jpg';
+import siege3 from './siege3.jpg';
+import siege4 from './siege4.jpg';
+import poussette from './poussette.jpg';
+import poussette2 from './poussette2.jpg';
+import poussette3 from './poussette3.jpg';
+import poussette4 from './poussette4.jpg';
+import visio from './visio.jpg';
+import visio2 from './visio2.jpg';
+import visio3 from './visio3.jpg';
+import visio4 from './visio4.jpg';
+import transat from './transat.jpg';
+import transat2 from './transat2.jpg';
+import transat3 from './transat3.jpg';
+import transat4 from './transat4.jpg';
+import portebb from './portebb.jpg';
+import portebb2 from './portebb2.jpg';
+import portebb3 from './portebb3.jpg';
+import portebb4 from './portebb4.jpg';
+import term from './term.jpg';
+import term2 from './term2.jpg';
+import term3 from './term3.jpg';
+import term4 from './term4.jpg';
+import veil from './veil.jpg';
+import veil2 from './veil2.jpg';
+import veil3 from './veil3.jpg';
+import veil4 from './veil4.jpg';
+import lanolin from './lanolin.jpg';
+import lanolin3 from './lanolin3.jpg';
+import lanolin2 from './lanolin2.jpg';
+import lanolin4 from './lanolin6.jpg';
+
+export const data = [
+    {
+        id : 1,
+        gifty : "Un Lit bébé",
+        image : [lit3, lit2, lit1, lit4],
+        like : 25
+    },
+    {
+        id : 2,
+        gifty : "Une Table/Commode à langer",
+        image : [table1, table2, table3, table4],
+        like : 12
+    },
+    {
+        id : 3,
+        gifty : "Un siège-auto",
+        image : [siege2, siege1, siege3, siege4],
+        like : 123
+    },
+    {
+        id : 4,
+        gifty : "Une Poussette",
+        image : [poussette3, poussette, poussette2, poussette4],
+        like: 254
+    },
+    {
+        id : 5,
+        gifty : "Un Babyphone/Visiophone",
+        image : [visio, visio3, visio2 , visio4],
+        like : 38
+    },
+    {
+        id : 6,
+        gifty : "Un transat",
+        image : [transat, transat3, transat2, transat4],
+        like : 29
+    },
+    {
+        id : 7,
+        gifty : "Un Porte-bébé/écharpe de portage",
+        image : [portebb, portebb3, portebb2, portebb4],
+        like : 145
+    },
+    {
+        id : 8,
+        gifty : "Un Thermomètre de bain",
+        image : [term2, term3, term, term4],
+        like : 58
+    },
+    {
+        id : 9,
+        gifty : "Une veilleuse",
+        image : [veil, veil2, veil3, veil4],
+        like : 61
+    },
+    {
+        id : 10,
+        gifty : "Lanoline pure",
+        image : [lanolin, lanolin2, lanolin3, lanolin4],
+        like : 112
+    }
+    
+]
